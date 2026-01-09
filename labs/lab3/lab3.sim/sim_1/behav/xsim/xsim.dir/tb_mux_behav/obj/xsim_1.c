a0f65fdc045d1ef2f0d741e394ed55af259b7898
stefan-zugravel <stefanzugravel@gmail.com>
2026-01-09 18:12:42 +0100

    added vcd and tcl

--- labs/lab3/lab3.sim/sim_1/behav/xsim/xsim.dir/tb_mux_behav/obj/xsim_1.c
+++ labs/lab3/lab3.sim/sim_1/behav/xsim/xsim.dir/tb_mux_behav/obj/xsim_1.c
@@ -55,21 +55,19 @@
 typedef void (*funcp)(char *, char *);
 extern int main(int, char**);
 IKI_DLLESPEC extern void execute_22(char*, char *);
-IKI_DLLESPEC extern void execute_27(char*, char *);
+IKI_DLLESPEC extern void execute_25(char*, char *);
 IKI_DLLESPEC extern void execute_21(char*, char *);
 IKI_DLLESPEC extern void execute_24(char*, char *);
-IKI_DLLESPEC extern void execute_25(char*, char *);
-IKI_DLLESPEC extern void execute_26(char*, char *);
 IKI_DLLESPEC extern void transaction_0(char*, char*, unsigned, unsigned, unsigned);
 IKI_DLLESPEC extern void vhdl_transfunc_eventcallback(char*, char*, unsigned, unsigned, unsigned, char *);
-funcp funcTab[8] = {(funcp)execute_22, (funcp)execute_27, (funcp)execute_21, (funcp)execute_24, (funcp)execute_25, (funcp)execute_26, (funcp)transaction_0, (funcp)vhdl_transfunc_eventcallback};
-const int NumRelocateId= 8;
+funcp funcTab[6] = {(funcp)execute_22, (funcp)execute_25, (funcp)execute_21, (funcp)execute_24, (funcp)transaction_0, (funcp)vhdl_transfunc_eventcallback};
+const int NumRelocateId= 6;
 
 void relocate(char *dp)
 {
-	iki_relocate(dp, "xsim.dir/tb_mux_behav/xsim.reloc",  (void **)funcTab, 8);
-	iki_vhdl_file_variable_register(dp + 2976);
-	iki_vhdl_file_variable_register(dp + 3032);
+	iki_relocate(dp, "xsim.dir/tb_mux_behav/xsim.reloc",  (void **)funcTab, 6);
+	iki_vhdl_file_variable_register(dp + 2840);
+	iki_vhdl_file_variable_register(dp + 2896);
 
 
 	/*Populate the transaction function pointer field in the whole net structure */
